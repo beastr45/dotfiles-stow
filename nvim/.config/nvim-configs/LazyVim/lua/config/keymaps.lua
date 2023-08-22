6cb36d24bd92bae7bc60a231dc4d7bce3a196fac
bear blinschauer <bearweston07@gmail.com>
2023-08-22 12:15:21 -0500

    added tmux config +neovim integration

--- nvim/.config/nvim-configs/LazyVim/lua/config/keymaps.lua
+++ nvim/.config/nvim-configs/LazyVim/lua/config/keymaps.lua
@@ -3,7 +3,7 @@
 -- Add any additional keymaps here
 
 -- - [[ Basic Keymaps ]]
-local opts = { noremap = true, silent = false }
+local opts = { noremap = true, silent = true }
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 --part of remap space as leader key code
@@ -81,3 +81,9 @@ vim.keymap.set("n", "<leader>bw", ":bw <cr>", { desc = "close buffer" })
 
 -- set zenmode keymap
 vim.keymap.set("n", "<leader>z", ":ZenMode <CR>", opts)
+
+-- Move to window using the <ctrl> hjkl keys but with tmux extenstion
+vim.keymap.set("n", "<C-h>", "<cmd> TmuxNavigateLeft<CR>", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-j>", "<cmd> TmuxNavigateDown<CR>", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-k>", "<cmd> TmuxNavigateUp<CR>", { desc = "Go to upper window", remap = true })
+vim.keymap.set("n", "<C-l>", "<cmd> TmuxNavigateRight<CR>", { desc = "Go to right window", remap = true })
--- nvim/.config/nvim-configs/LazyVim/lua/plugins/custom.lua
+++ nvim/.config/nvim-configs/LazyVim/lua/plugins/custom.lua
@@ -1,4 +1,8 @@
 --custom lazyvim independent plugins go here
 return {
-  "Pocco81/auto-save.nvim",
+    "Pocco81/auto-save.nvim",
+    {
+        "christoomey/vim-tmux-navigator",
+        lazy = false,
+    },
 }
