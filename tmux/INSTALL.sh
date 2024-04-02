86ee413aeda5f134ea2d6957abc957389764a143
Bear BlinSchauer <bearweston07@gmail.com>
2024-04-01 22:10:16 -0500

    style: add comment to avoid the confusion from before

--- tmux/INSTALL.sh
+++ tmux/INSTALL.sh
@@ -3,8 +3,11 @@
 echo "Script is running."
 
 # Create the parent directories recursively if they don't exist
-mkdir -p ~/.config/tmux/plugins
+mkdir -p ~/.config/tmux/plugins/
 
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 
 echo "Script execution complete."
+# ╭──────────────────────────────────────────────────────────╮
+# │               !Important! press <leader> I               │
+# ╰──────────────────────────────────────────────────────────╯
